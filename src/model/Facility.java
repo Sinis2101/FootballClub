0ba7a1593209a98de5b1d4c4f2fc08c6518ba169
Sinis2101 <sinisterra2101@gmail.com>
2020-12-03 10:27:19 -0500

    Deleted OfficeArea Class and made it a relation between Facility and Manager.

--- src/model/Facility.java
+++ src/model/Facility.java
@@ -4,13 +4,13 @@ public class Facility {
 
     private DressingRoom dressingRoomA;
     private DressingRoom dressingRoomB;
-    private OfficeArea officeArea;
+    private Manager[][] officeArea;
 
     public Facility(Team teamA, Team teamB) {
 
         dressingRoomA = new DressingRoom(7, 7, teamA);
         dressingRoomB = new DressingRoom(7, 6, teamB);
-        officeArea = new OfficeArea(6, 6);
+        officeArea = new Manager[6][6];
 
     }
 }
